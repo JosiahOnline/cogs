59b82a0961b8285e71c028cc67a2798f8126aa18
JosiahOnline <josiahriverflow@hotmail.com>
2018-07-09 21:18:39 +0800

    Updates Basketball App
    1) Added StopWatch
    2) Changed Favicon png

--- News/Ongoing/app/score.js
+++ News/Ongoing/app/score.js
@@ -121,6 +121,10 @@ function select_random() {
             chooseTeam[i].style.display = "block";
         }
         z.style.display = "none";
+        let y = document.getElementById("frm1");
+        y.style.display = "none";
+        let x = document.getElementById("enterName");
+        x.style.display = "none";
     }
 
     //randomly select first 5 players from var players
@@ -145,8 +149,233 @@ function myFunction1() {
     }
     let z = document.getElementById("button2");
     z.style.display = "none";
+    let w = document.getElementById("yourFate");
+    w.style.display = "block";
+    let v = document.getElementById("chrono");
+    v.style.display = "block";
+    let k = document.getElementById("clockSetting");
+    k.style.display = "flex";
 }
 
+
+let cnv = document.getElementById("chrono"); // canvas
+let h = document.getElementById("hour");
+let m = document.getElementById("minute");
+let s = document.getElementById("second");
+//taille du canvas égal au dimension de l'écran
+cnv.width = 200; 
+cnv.height = 200;
+let ctx = cnv.getContext("2d");
+//initialisation de letiable
+let status = 'w'; // status : w = waiting ; p = pause ; s = stop ; r = run 
+let fontType = "Arial"
+let time = dTime = 0;
+let duration = 60000;
+
+let x = cnv.width / 2;
+let y = cnv.height / 2;
+let r = Math.min(cnv.width, cnv.height) / 2; // rayon = la plus petite dimension de l'écran
+let e = r / 8; //epaisseur du contour du cercle en fonction du rayon
+r = r - e; // on enlève l'épaisseur du trait pour ne pas dépasser la taille du canvas
+let fontSize = computeFontSize("00:00:00", fontType); //adapter le texte du chrono au rayon du cercle
+let font = fontSize + "px " + fontType;
+/*
+ *
+ * fonctions de calcul
+ *
+ */
+function computeDuration() {
+    let d = 0;
+    if (h && m && s) {
+        duration = h.value * 3600000 + m.value * 60000 + s.value * 1000;
+    }
+}
+
+function computeFontSize(text, fontface) {
+    let maxWidth = r * 2 * 0.8;
+    let fontsize = 300;
+    do {
+        fontsize--;
+        ctx.font = fontsize + "px " + fontface;
+    } while (ctx.measureText(text).width > maxWidth)
+    return fontsize;
+}
+/*
+ *
+ * fonctions de formattage
+ *
+ */
+function formatTime(time) {
+    let tmp = Math.floor(time / 1000);
+    let second = tmp % 60;
+    if (second < 10) {
+        second = '0' + second
+    };
+    tmp = Math.floor(tmp / 60);
+    let minute = tmp % 60;
+    if (minute < 10) {
+        minute = '0' + minute
+    };
+    tmp = Math.floor(tmp / 60);
+    let hour = tmp;
+    if (hour < 10) {
+        hour = '0' + hour
+    };
+    return hour + ':' + minute + ':' + second;
+}
+/*
+ *
+ * fonctions création d'objet
+ *
+ */
+function createCircle() {
+    return {
+        centerX: cnv.width / 2,
+        centerY: cnv.height / 2,
+        radius: Math.min(cnv.width, cnv.height) / 2 - (Math.min(cnv.width, cnv.height) / 2) / 8,
+        startAngle: 1.5 * Math.PI,
+        endAngle: 3.5 * Math.PI,
+        lineWidth: (Math.min(cnv.width, cnv.height) / 2) / 8,
+        color: 'rgba(255,255,255,0.2)',
+    };
+}
+/*
+ *
+ * fonctions de canvas et dessins
+ *
+ */
+function clearCanvas(context, canvas) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawCircle(myCircle, ctx) {
+    ctx.beginPath();
+    ctx.arc(myCircle.centerX, myCircle.centerY, myCircle.radius, myCircle.startAngle, myCircle.endAngle);
+    ctx.lineWidth = myCircle.lineWidth;
+    ctx.strokeStyle = myCircle.color;
+    ctx.stroke();
+}
+
+function drawChrono(time, font, ctx) {
+    ctx.font = font;
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText(formatTime(time), cnv.width / 2, cnv.height / 2 + fontSize / 4);
+}
+/*
+ *
+ * function animation
+ *
+ */
+function animate() {
+    let startTime = +new Date();
+    let step = function () {
+        if (status != 'p' && status != 's') {
+            time = +new Date() - startTime + dTime;
+            let perc = time / duration;
+            if (perc > 1) {
+                perc = 1;
+            }
+            let angle = 2 * Math.PI * perc;
+            clearCanvas(ctx, cnv);
+            let color = 'green';
+            if (perc > (3 / 4 + 2 / 12)) {
+                color = 'red';
+            } else if (perc > (3 / 4 + 1 / 12)) {
+                color = 'orange';
+            } else if (perc > 3 / 4) {
+                color = 'yellow';
+            }
+            progCircle = createCircle();
+            progCircle.endAngle = progCircle.startAngle + angle;
+            progCircle.color = color;
+            whiteCircle = createCircle();
+            drawChrono(time, font, ctx);
+            drawCircle(whiteCircle, ctx);
+            drawCircle(progCircle, ctx);
+            requestAnimationFrame(step)
+        } else {
+            status = 'w';
+        }
+    }
+    step();
+};
+/*
+ *
+ * Events listener
+ *
+ */
+document.getElementById("runBtn").addEventListener("click", run);
+document.getElementById("pauseBtn").addEventListener("click", pause);
+document.getElementById("stopBtn").addEventListener("click", stop);
+h.addEventListener("change", computeDuration);
+m.addEventListener("change", computeDuration);
+s.addEventListener("change", computeDuration);
+
+function init() {
+    drawCircle(createCircle(), ctx);
+    drawChrono(0, font, ctx)
+}
+
+function run() {
+    status = 'r';
+    animate();
+}
+
+function pause() {
+    status = 'p';
+    dTime = time;
+}
+
+function stop() {
+    status = 's';
+    dTime = 0;
+    clearCanvas(ctx, cnv);
+    init();
+}
+
+init();
+//let status = 0; //0:stop 1:running
+//    let time = 0;
+//                    
+//    function start(){
+//        status = 1;
+//        document.getElementById("startBtn").disabled = true;
+//        timer();
+//    }
+//    function stop(){
+//        status = 0;
+//        document.getElementById("startBtn").disabled = false;
+//    }
+//    function reset(){
+//        status = 0;
+//        time = 0;
+//        document.getElementById('timerLabel').innerHTML = '00:00:00';
+//        document.getElementById("startBtn").disabled = false;
+//    }
+//    function timer(){
+//        if(status == 1){
+//            setTimeout(function(){
+//                time++;
+//                let min = Math.floor(time/100/60);
+//                let sec = Math.floor(time/100);
+//                let mSec = time % 100;
+//                    
+//                if(min < 10) {
+//                    min = "0" + min;
+//                }
+//                if(sec >= 60) {
+//                    sec = sec % 60;
+//                }
+//                if(sec < 10) {
+//                    sec = "0" + sec;
+//                }
+//                    
+//                document.getElementById('timerLabel').innerHTML = min + ":" + sec + ":" + mSec;
+//                timer();
+//            }, 10);
+//        }
+//    }
 //function select_captain(team) {
 //    //from the list, randomly choose a captain.
 //    //return the chosen one to the team list.
